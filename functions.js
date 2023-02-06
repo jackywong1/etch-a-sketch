c5e6adba1c7b9aeacabf7d9fb5e1f8ea3b737679
Jacky Wong <z5115789@student.unsw.edu.au>
2023-02-06 16:25:36 +1100

    added a button that refreshes the state of the grid

--- functions.js
+++ functions.js
@@ -1,7 +1,11 @@
 const numOfGrids = 256;
 
+
+// creates 16 x 16 grid
 let divElement = document.createElement('div');
 divElement.classList.add('container');
+let divElementTwo = document.createElement('div');
+divElementTwo.classList.add('button-container');
 
 for (let i = 0; i < numOfGrids; i++) {
     let gridElement = document.createElement('div');
@@ -10,3 +14,29 @@ for (let i = 0; i < numOfGrids; i++) {
 }
 
 document.body.appendChild(divElement);
+
+const gridSquares = document.querySelectorAll('.gridSquare');
+// console.log(gridSquares)
+gridSquares.forEach((gridSquare) => {
+    gridSquare.addEventListener('mouseover', e => {
+        e.target.classList.add('active');
+    })
+})
+
+
+// creates refresh button to reset state of grids
+let refreshButtonElement = document.createElement('button');
+refreshButtonElement.classList.add('refresh-button');
+refreshButtonElement.textContent = 'refresh';
+divElementTwo.appendChild(refreshButtonElement);
+
+document.body.appendChild(divElementTwo);
+
+const allGrids = document.querySelectorAll('.gridSquare');
+const refreshButton = document.querySelector('.refresh-button');
+
+refreshButton.addEventListener('click', refreshStateOfGrids);
+
+function refreshStateOfGrids () {
+    allGrids.forEach((grid) => grid.classList.remove('active'));
+}
